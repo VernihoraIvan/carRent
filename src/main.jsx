9fa3cf5fbe35aa65f220bb535e0cc8b5b4c289cc
VernihoraIvan <vernihorai@gmail.com>
2023-11-06 00:41:07 +0100

    ds

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/carRent">
       <App />
     </BrowserRouter>
   </Provider>
