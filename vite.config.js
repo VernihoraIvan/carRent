03651f802e8907c0d260282dfe3ea84facac5790
VernihoraIvan <vernihorai@gmail.com>
2023-11-06 15:23:24 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-// import path from "node:path";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(__dirname, "./src"),
-  //   },
-  // },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [react(), svgr()],
   base: "./",
 });
